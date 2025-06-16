2cf2e95932954b6aaf757b2a9fd32b34e7dcb62f
tenzzly <renato.calvo2@gmail.com>
2025-06-16 11:50:00 -0500

    fix: main tallerazo

--- src/main/java/com/acme/tallerazo/TallerazoApplication.java
+++ src/main/java/com/acme/tallerazo/TallerazoApplication.java
@@ -2,8 +2,10 @@ package com.acme.tallerazo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TallerazoApplication {
 
     public static void main(String[] args) {
