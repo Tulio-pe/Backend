74a54e63e7fd35632194e2a28c76ef1abda9f410
tenzzly <renato.calvo2@gmail.com>
2025-06-18 12:12:06 -0500

    feat(services):add queries for services

---
+++ src/main/java/com/acme/tallerazo/workShopManagment/domain/model/queries/GetServiceByNameQuery.java
@@ -0,0 +1,7 @@
+package com.acme.tallerazo.workShopManagment.domain.model.queries;
+
+import com.acme.tallerazo.workShopManagment.domain.model.valueobjects.WorkServices;
+
+public record GetServiceByNameQuery(WorkServices workServices) {
+
+}
