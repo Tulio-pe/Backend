d1e3171b5a51e8aa77c9a3e15650c0086ab3d27b
tenzzly <renato.calvo2@gmail.com>
2025-06-19 18:53:36 -0500

    chore: remove unused classes for project

--- src/main/java/com/acme/tallerazo/iam/domain/services/RoleCommandService.java
+++
@@ -1,9 +0,0 @@
-package com.acme.tallerazo.iam.domain.services;
-
-import com.acme.tallerazo.iam.domain.model.commands.SeedRolesCommand;
-
-;
-
-public interface RoleCommandService {
- void handle(SeedRolesCommand seedRolesCommand);
-}
