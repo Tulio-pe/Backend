cfff427b2bc9fa822a31e1fd412a6f5430a95e2e
Necxuz18 <u202210938@upc.edu.pe>
2025-06-22 14:03:31 -0500

    chore: remove OpenApiConfiguration class and its methods

--- src/main/java/com/acme/tallerazo/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/tallerazo/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -17,51 +17,5 @@ import org.springframework.context.annotation.Configuration;
 * Define metadatos de la aplicación y configuración de seguridad JWT para Swagger UI.
 */
 
-@Configuration
-public class OpenApiConfiguration {
-    // Properties
-    @Value("${spring.application.name}")
-    String applicationName;
 
-    @Value("${documentation.application.description}")
-    String applicationDescription;
-
-    @Value("${documentation.application.version}")
-    String applicationVersion;
-
-    // Methods
-
-    @Bean
-    public OpenAPI learningPlatformOpenApi() {
-        // General configuration
-        var openApi = new OpenAPI();
-        openApi
-                .info(new Info()
-                        .title(this.applicationName)
-                        .description(this.applicationDescription)
-                        .version(this.applicationVersion)
-                        .license(new License().name("Apache 2.0")
-                                .url("https://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("ACME Learning Platform wiki Documentation")
-                        .url("https://acme-learning-platform.wiki.github.io/docs"));
-
-        // Add a security scheme
-
-        final String securitySchemeName = "bearerAuth";
-
-        openApi.addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-
-        // Return the OpenAPI configuration object with all the settings
-
-        return openApi;
-    }
 }
