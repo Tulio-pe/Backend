933f8308add291cdaa2aa39fee8c1d9b287074a3
Necxuz18 <u202210938@upc.edu.pe>
2025-06-22 14:06:26 -0500

    feat: add CarQueryService interface with handle methods for car queries

--- src/main/java/com/acme/tallerazo/RepairManagement/domain/services/CarQueryService.java
+++ src/main/java/com/acme/tallerazo/RepairManagement/domain/services/CarQueryService.java
@@ -1,1 +1,13 @@
+package com.acme.tallerazo.RepairManagement.domain.services;
 
+import com.acme.tallerazo.RepairManagement.domain.model.Queries.GetAllCarQuery;
+import com.acme.tallerazo.RepairManagement.domain.model.Queries.GetCarByPlaceQuery;
+import com.acme.tallerazo.RepairManagement.domain.model.aggregates.Car;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarQueryService {
+    List<Car> handle (GetAllCarQuery query);
+    Optional<Car>handle(GetCarByPlaceQuery query);
+}
